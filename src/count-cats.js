4b10d4825223dafa6f3504de7f4c18b389ab29f3
BSKOM <b.shelestua@gmail.com>
2021-03-06 22:38:18 +0200

    feat: count-cat - optimized algothitm, optimized function to one string

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,3 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(matrix) {  
-  const s = ('!!' + matrix.flat(Infinity).join('!!!') + '!!').split('!^^!').length 
-  return s ? s - 1 : s
-};
+module.exports = (matrix) => ('!!^^!!' + matrix.flat(Infinity).join('!!') + '!!').split('!^^!').length - 2
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,8 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  
-  // throw new CustomError('Not implemented');
+module.exports = function repeater( str, options ) {
+  // if ( options === undefined || str === undefined || str.lenght === 0 ) return false 
+  // return true
+
+  throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
   
