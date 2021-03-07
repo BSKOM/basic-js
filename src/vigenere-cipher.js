5f2d005cb4703bfa01bfef26ce7f3c3d05c476fb
BSKOM <b.shelestua@gmail.com>
2021-03-07 21:05:23 +0200

    feat: vigenere-cipher add decrypt method

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -16,10 +16,21 @@ class VigenereCipheringMachine {
     // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    let res = []
+    let n = 0
+    for (let i = 0; i < str.length; i++) {
+      if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
+        res += String.fromCharCode((str.charCodeAt(i) - key.toUpperCase().charCodeAt(n) + 26) % 26 + 'A'.charCodeAt(0))
+        n = (n + 1) % key.length
+      } else {
+        res += str[i]
+      }
+    }
+    return res
   }
+  // throw new CustomError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = VigenereCipheringMachine;
