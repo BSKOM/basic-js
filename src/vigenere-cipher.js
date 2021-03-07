cd45cb4eac8b17210941c00dda28d03d31a9388a
BSKOM <b.shelestua@gmail.com>
2021-03-07 17:51:52 +0200

    feat: vigenere-cipher add enctypt method

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,10 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  encrypt(str, key) {
+    let res = []
+    let n = 0
+    for (let i = 0; i < str.length; i++) {
+      if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
+        res += String.fromCharCode((str.toUpperCase().charCodeAt(i) - 'A'.charCodeAt(0) + key.toUpperCase().charCodeAt(n) - 'A'.charCodeAt(0)) % 26 + 'A'.charCodeAt(0))
+        n = (n + 1) % key.length
+      } else {
+        res += str[i]
+      }
+    }
+    return res
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
+  }
   decrypt() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
