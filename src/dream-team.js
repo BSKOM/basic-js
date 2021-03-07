9f99e60dcba5837fd55f5bfddf442082aab7c913
BSKOM <b.shelestua@gmail.com>
2021-03-07 23:05:10 +0200

    feat: dream-team add release v 1.0

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam( members ) {
+  return members.map(item => item.trim()[0].toUpperCase()).sort().join('');
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
