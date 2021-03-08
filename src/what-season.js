9c182a1f07bfb7c3686f3a36d7f11084df3dbf82
BSKOM <b.shelestua@gmail.com>
2021-03-08 12:09:26 +0200

    feat: compensated -1 in month number

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason( date ) {
-  return ['winter', 'spring', 'summer', 'autumn'][Math.floor((date.getMonth() / 12 * 4)) % 4]
+  return ['winter', 'spring', 'summer', 'autumn'][Math.floor(((date.getMonth()+1) / 12 * 4)) % 4]
 };
