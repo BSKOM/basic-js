13b2e394defa1cda599c37dfa6d3d84ae85db7c0
BSKOM <b.shelestua@gmail.com>
2021-03-08 14:25:00 +0200

    feat: add check on empty date and return message about this

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason( date ) {
-  return ['winter', 'spring', 'summer', 'autumn'][Math.floor(((date.getMonth()+1) / 12 * 4)) % 4]
-};
+module.exports = function getSeason( date = 'empty') {
+  if (date == 'empty') return 'Unable to determine the time of year!'
+  if (date instanceof Date){
+    if (Object.prototype.toString.call(date) !== '[object Date]') throw new error('Not date')
+    return ['winter', 'spring', 'summer', 'autumn'][Math.floor(((date.getMonth() + 1) / 12 * 4)) % 4] 
+  }
+  throw new error('Not date')
+}
