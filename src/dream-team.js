3aca24322280129d6e702fc8ca32c295d0014852
BSKOM <b.shelestua@gmail.com>
2021-03-08 08:02:58 +0200

    feat: add filter type string, add сheck type array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam( members ) {
-  return members.map(item => item.trim()[0].toUpperCase()).sort().join('');
+  // return members.map(item => item.trim()[0].toUpperCase()).sort().join('');
+  if (Array.isArray(members)) {
+    return members.filter((number) => (typeof number === 'string')).map(item => item.trim()[0].toUpperCase()).sort().join('');
+  }
+  return false
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
