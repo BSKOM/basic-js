03eeb1352dad3399e0c3ab6ade22478f5c3e116f
BSKOM <b.shelestua@gmail.com>
2021-03-08 08:15:41 +0200

    fix: fix minor syntax

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,11 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam( members ) {
-  // return members.map(item => item.trim()[0].toUpperCase()).sort().join('');
-  if (Array.isArray(members)) {
-    return members.filter((number) => (typeof number === 'string')).map(item => item.trim()[0].toUpperCase()).sort().join('');
-  }
-  return false
-  // throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  return Array.isArray(members) ? members.filter((number) => (typeof number === 'string')).map(item => item.trim()[0].toUpperCase()).sort().join('') : false
+}
