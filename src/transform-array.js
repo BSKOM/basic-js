58f24fdc997f55b5e5f0f7e58905517e926e37b7
BSKOM <b.shelestua@gmail.com>
2021-03-09 12:50:59 +0200

    fix: fix check simple arrays

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,11 +2,11 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform( arr ) {
   // throw new CustomError('Not implemented');
-  if (!arr.length) return []
+  if (!arr.length) return arr
 
   if (Array.isArray(arr) && arr.constructor === Array) {
-    if (!(arr.includes('--discard-next') || arr.includes('--discard-next') || arr.includes('--discard-next') || arr.includes('--discard-next'))) return arr
-  }
+    if (!(arr.includes('--discard-next') || arr.includes('--discard-prev') || arr.includes('--double-next') || arr.includes('--double-prev'))) return arr
+   }
   else throw error('Not array')
   // remove line with error and write your code here
 };
