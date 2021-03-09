e4b66a87a52239dab6ebd9fe154b8c2394993b9d
BSKOM <b.shelestua@gmail.com>
2021-03-09 16:27:34 +0200

    feat: add advanced sequence

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,30 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform( arr ) {
-  // throw new CustomError('Not implemented');
-  // if (!arr.length) return arr
-
-  // if (Array.isArray(arr) && arr.constructor === Array) {
-  //   if (!(arr.includes('--discard-next') || arr.includes('--discard-prev') || arr.includes('--double-next') || arr.includes('--double-prev'))) return arr
-  //  }
-  // else throw error('Not array')
+module.exports = function transform(arr) {
   let res = []
-  let i = 0
   if (!arr.length) return arr
   if (Array.isArray(arr) && arr.constructor === Array) {
-
     if (!(arr.includes('--discard-next') || arr.includes('--discard-prev') || arr.includes('--double-next') || arr.includes('--double-prev'))) return arr
-    for (let value of arr) {
-      if (((value === '--discard-prev') && (i === 0)) || ((value === '--discard-next') && i === (arr.length - 1)) || 
-        ((value === '--double-prev') && (i === 0)) || ((value === '--double-next') && i === (arr.length - 1)) ) {
-      } else {
-        res.push(value)
+    for (let i = 0; i < arr.length;) {
+      if (((arr[i] === '--discard-prev') && (i === 0))
+        || ((arr[i] === '--discard-next') && (i === (arr.length - 1)))
+        || ((arr[i] === '--double-prev') && (i === 0))
+        || ((arr[i] === '--double-next') && (i === (arr.length - 1)))
+      ) {
+      } else if ((arr[i] === '--discard-next') && (arr[i + 2] === '--double-prev')) {
+        i += 2
+      } else if ((arr[i] === '--double-next') && (arr[i + 2] === '--double-prev')) {
+        res.push(arr[i + 1])
+        res.push(arr[i + 1])
+        res.push(arr[i + 1])
+        i += 2
+      } else if ((arr[i] === '--discard-next') && (arr[i + 2] === '--discard-prev')) {
+        i += 2
+      } else if ((arr[i] === '--double-next') && (arr[i + 2] === '--discard-prev')) {
+        res.push(arr[i + 1])
+        i += 2
+      }
+      else {
+        res.push(arr[i])
       }
       i += 1
     }
     return res
   }
-
-  else throw error('Not array')
-  // remove line with error and write your code here
 };
