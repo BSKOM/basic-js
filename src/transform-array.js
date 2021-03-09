5e5e864626446302a0ba095165041b5c13be33ce
BSKOM <b.shelestua@gmail.com>
2021-03-09 12:04:40 +0200

    feat: add check on empty array and array type

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform( arr ) {
+  // throw new CustomError('Not implemented');
+  if (!arr.length) return []
+
+  if (Array.isArray(arr) && arr.Array.constructor === Array) {
+
+  }
+  else throw error('Not date')
   // remove line with error and write your code here
 };
